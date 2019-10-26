1dbfe00419cf2324969f6673ea49b844a9c396dd
Jahin Ahmed <jahinjin@lawn-143-215-89-207.lawn.gatech.edu>
2019-10-26 14:40:28 -0400

    more auth functionality

---
+++ functions/services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from  'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyC8JySEcaBYAiS0HUk0CWkkhUM11cwrixM",
+    authDomain: "superhackbros-b8a46.firebaseapp.com",
+    databaseURL: "https://superhackbros-b8a46.firebaseio.com",
+    projectId: "superhackbros-b8a46",
+    storageBucket: "superhackbros-b8a46.appspot.com",
+    messagingSenderId: "1003110864941",
+    appId: "1:1003110864941:web:40df1cc0d4fa1a5c6240a4",
+    measurementId: "G-WLN5JJM3V9"
+  };
+
+  !firebase.apps.length ? firebase.initializeApp(config): '';
+
+  export const auth = firebase.auth();
+    
