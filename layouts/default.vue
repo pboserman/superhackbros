0d1c11e52fdd8082bad025e3a494eeb61bffae01
Pablo Boserman <pboserman3@gatech.edu>
2019-10-26 17:22:34 -0400

    Adds toolbar
    Signed-off-by: Pablo Boserman <pboserman3@gatech.edu>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
+  <div class="has-navbar-fixed-bottom">
     <nuxt />
+    <toolbar />
   </div>
 </template>
+
+<script>
+import Toolbar from '~/components/Toolbar.vue'
+
+export default {
+  components: {
+    Toolbar
+  }
+}
+</script>
 
 <style></style>
